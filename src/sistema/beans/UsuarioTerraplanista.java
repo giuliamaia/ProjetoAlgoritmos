5716a19a17890ae110c9d846e19f1876b3f46ff3
Rodrigues <Rodrigues@DESKTOP-6HRI2S5>
2019-11-28 22:17:51 -0300

    Beans de teste

---
+++ src/sistema/beans/UsuarioTerraplanista.java
@@ -0,0 +1,14 @@
+package sistema.beans;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class UsuarioTerraplanista {
+	private List<UsuarioTerraplanista> amigos;
+	private List<String> interesses;
+	private LocalDate dataNascimento;
+	private LocalDateTime horaCriaçãoConta;
+	
+	
+}
--- src/sistema/gui/Main.java
+++ src/sistema/gui/Main.java
@@ -1,4 +1,4 @@
-package Pacote;
+package sistema.gui;
 import java.io.File;
 import java.net.URL;
 
