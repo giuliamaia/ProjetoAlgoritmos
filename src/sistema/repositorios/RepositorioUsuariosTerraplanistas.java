60c60079636301d42973a57c991b696a82b9bf6f
Rodrigues <Rodrigues@DESKTOP-6HRI2S5>
2019-11-28 22:39:44 -0300

    repositorios

---
+++ src/sistema/repositorios/RepositorioUsuariosTerraplanistas.java
@@ -0,0 +1,9 @@
+package sistema.repositorios;
+
+import java.util.List;
+
+import sistema.beans.UsuarioTerraplanista;
+
+public class RepositorioUsuariosTerraplanistas {
+	private List<UsuarioTerraplanista> usuarios;
+}
