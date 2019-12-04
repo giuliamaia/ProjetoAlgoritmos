744ff814a77543026d1a19b5eb2052a223a21b33
Rodrigues <Rodrigues@DESKTOP-6HRI2S5>
2019-12-04 00:01:26 -0300

    Bregnaits

---
+++ src/sistema/beans/Teste.java
@@ -0,0 +1,33 @@
+package sistema.beans;
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+
+
+public class Teste {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+
+		Graph grafo = new MultiGraph("Usuários");
+		grafo.setStrict(false);
+		grafo.setAutoCreate(true);
+		grafo.addEdge("SaGiu", "Sabrina", "Giulia");
+		grafo.addEdge("SaGu", "Rodrigues", "Kakarot");
+		grafo.addEdge("Saiu", "Sabrina", "Kakarot");
+		grafo.addEdge("Giu", "Rodrigues", "Giulia");
+		grafo.addEdge("Bunda", "Sabrina", "Furico");
+		grafo.addEdge("Giiu", "Romulo", "Copo");
+		grafo.addEdge("Bunda", "Copo", "Rodrigues");
+		grafo.addEdge("Bundaa", "Corpo", "Copo");
+		grafo.addAttribute("ui.stylesheet", "url('/CSS/grafo.css')");
+		grafo.display();
+		
+		//Viewer visualizador = grafo.display();
+		//View visao = visualizador.getDefaultView();
+		//visao.getCamera().setViewCenter(4, 2, 1);
+		//visualizador.close();
+	}
+}
--- src/sistema/gui/controladores/TelaLoginController.java
+++ src/sistema/gui/controladores/TelaLoginController.java
@@ -1,52 +1,32 @@
 package sistema.gui.controladores;
 
-import java.io.IOException;
-
-import com.google.common.graph.*;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import sistema.beans.Grafo;
 
 public class TelaLoginController {
-	Grafo grafo = Grafo.getInstance();
-	String texto = "";
-	 @FXML
-	    private Label label;
+	@FXML
+	private Label label;
 
-	    @FXML
-	    private TextField tfVertice;
+    @FXML
+    private TextField tfVertice;
 
-	    @FXML
-	    private TextField tfa2;
+    @FXML
+    private TextField tfa2;
 
-	    @FXML
-	    private TextField tfa1;
-
+    @FXML
+    private TextField tfa1;
 
     @FXML
     void addAresta(ActionEvent event) {
-    	try {
-			grafo.addAresta(tfa1.getText(), tfa2.getText());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	
     }
 
     @FXML
     void addVertice(ActionEvent event) {
-    	System.out.println(tfVertice.getText());
-    	try {
-			grafo.addVertice(tfVertice.getText());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	texto = texto.concat(" -- " + tfVertice.getText());
-    	label.setText(texto);
+    	
     }
 
 }
