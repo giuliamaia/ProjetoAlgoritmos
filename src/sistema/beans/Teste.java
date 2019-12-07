900559a297b207687253f96f5dd5701df3ed78f8
Rodrigues <Rodrigues@DESKTOP-6HRI2S5>
2019-12-07 16:03:34 -0300

    Resolvendo alguns bugs

--- src/sistema/beans/Teste.java
+++ src/sistema/beans/Teste.java
@@ -1,15 +1,18 @@
 package sistema.beans;
 
 import java.net.MalformedURLException;
-import java.time.LocalDate;
 import sistema.controlador.Controlador;
 
 
 public class Teste {
 	public static void main(String[] args) throws MalformedURLException {
+		
 		Controlador controlador = Controlador.getInstancia();
 		Grafo grafo = Grafo.getInstancia();
-		controlador.adicionarUsuario(new UsuarioTerraplanista(null, "Isntyou", "isnobb", "123", LocalDate.of(2001, 10, 04), null));
+		//controlador.adicionarUsuario(new UsuarioTerraplanista(null, "Reginaldo", "isnobb", "123", LocalDate.of(2001, 10, 04), null));
+		for (int i = 0; i < controlador.getUsuarios().size(); i ++) {
+			System.out.println(controlador.getUsuarios().get(i).getNome());
+		}
 	    grafo.display();
 
 	}
