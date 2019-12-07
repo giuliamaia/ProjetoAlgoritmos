33bec1641f2bbdbfc0347c88148cdd1a7dd5b8ae
Rodrigues <Rodrigues@DESKTOP-6HRI2S5>
2019-12-06 22:20:26 -0300

    Alteracao no teste

--- src/sistema/beans/Teste.java
+++ src/sistema/beans/Teste.java
@@ -6,29 +6,32 @@ import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
 import org.graphstream.ui.view.Viewer;
 
+import javafx.scene.paint.Color;
+
 
 public class Teste {
 	public static void main(String[] args) throws MalformedURLException {
 		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
-
-		Graph grafo = new MultiGraph("Usuários");
-		File file = new File("CSS/grafo.css");
-		System.out.println(file.exists());
-		grafo.addAttribute("ui.stylesheet", "url('CSS/grafo.css')");
-		grafo.setStrict(false);
-		grafo.setAutoCreate(true);
-		grafo.addEdge("SaGiu", "Sabrina", "Giulia");
-		grafo.addEdge("SaGu", "Rodrigues", "Kakarot");
-		grafo.addEdge("Saiu", "Sabrina", "Kakarot");
-		grafo.addEdge("Giu", "Rodrigues", "Giulia");
-		grafo.addEdge("Bunda", "Sabrina", "Furico");
-		grafo.addEdge("Giiu", "Romulo", "Copo");
-		grafo.addEdge("Bunda", "Copo", "Rodrigues");
-		grafo.addEdge("Bundaa", "Corpo", "Copo");
-		
-		Viewer visualizador = grafo.display();
-		//View visao = visualizador.getDefaultView();
-		//visao.getCamera().setViewCenter(4, 2, 1);
-		//visualizador.close();
+	    SingleGraph graph = new SingleGraph("Use");
+	    graph.addNode("A");
+	    graph.addNode("B");
+	    graph.addNode("C");
+	    graph.addNode("D");
+	    graph.addNode("E");
+	    graph.addNode("F");
+	    graph.addNode(1+"");
+	    graph.addNode(2+"");
+	    graph.addEdge("AB", "A", "B");
+	    graph.addEdge(1+""+2+"", 1+"", 2+"");
+	    graph.addEdge("BC", "B", "C");
+	    graph.addEdge("CA", "C", "A");
+	    graph.addEdge("CD", "C", "D");
+	    graph.addEdge("DF", "D", "F");
+	    graph.addEdge("EF", "E", "F");
+	    graph.addEdge("DE", "D", "E");
+	    Node e1=graph.getNode("A");
+	    e1.addAttribute("ui.style", "text-alignment: at-right; text-padding: 3px, 2px; text-background-mode: rounded-box; text-background-color: #e09410; text-color: white; text-style: bold-italic; text-color: #FFF; text-offset: 5px, 0px;");
+	    e1.addAttribute("ui.label", "A");
+	    graph.display();
 	}
 }
