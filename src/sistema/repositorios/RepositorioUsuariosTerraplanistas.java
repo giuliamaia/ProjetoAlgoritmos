766ec5167fc4381459efabdaa0f1598dbcb46ee4
Rodrigues <Rodrigues@DESKTOP-6HRI2S5>
2019-12-06 22:27:07 -0300

    Serialização e estilo de nó

--- src/sistema/repositorios/RepositorioUsuariosTerraplanistas.java
+++ src/sistema/repositorios/RepositorioUsuariosTerraplanistas.java
@@ -1,5 +1,6 @@
 package sistema.repositorios;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import org.graphstream.graph.Node;
@@ -7,12 +8,16 @@ import org.graphstream.graph.Node;
 import sistema.beans.Grafo;
 import sistema.beans.UsuarioTerraplanista;
 
-public class RepositorioUsuariosTerraplanistas {
+public class RepositorioUsuariosTerraplanistas implements Serializable{
+	private static final long serialVersionUID = 1L;
 	private List<UsuarioTerraplanista> usuarios = new ArrayList<UsuarioTerraplanista>();
 	private Grafo grafo = Grafo.getInstancia();
 	public void adicionarUsuario(UsuarioTerraplanista usuario) {
 		this.usuarios.add(usuario);
 		grafo.addNode(usuario.getNome());
+		Node no = grafo.getNode(usuario.getNome());
+		no.addAttribute("ui.style", "text-alignment: at-right; text-padding: 3px, 2px; text-background-mode: rounded-box; text-background-color: #e09410; text-color: white; text-style: bold-italic; text-color: #FFF; text-offset: 5px, 0px;");
+		no.addAttribute("ui.label", usuario.getNome());
 	}
 	public void	removerUsuario(UsuarioTerraplanista usuario) {
 		this.usuarios.remove(usuario);
@@ -20,7 +25,6 @@ public class RepositorioUsuariosTerraplanistas {
 	}
 	public void editarUsuario(UsuarioTerraplanista usuarioVelho, UsuarioTerraplanista usuarioNovo) {
 		usuarioVelho = usuarioNovo;
-		Node no = grafo.getNode(usuarioVelho.getNome());
-		no.setAttribute(usuarioVelho.getNome(), usuarioNovo.getNome());
+		
 	}
 }
