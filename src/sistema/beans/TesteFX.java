94c03fa06a68644135270f3a7f04350f49d8edd0
Rodrigues <Rodrigues@DESKTOP-6HRI2S5>
2019-12-09 23:34:11 -0300

    Junção das duas funções de Sabrina

--- src/sistema/beans/TesteFX.java
+++ src/sistema/beans/TesteFX.java
@@ -1,5 +1,7 @@
 package sistema.beans;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import sistema.controlador.Controlador;
@@ -8,16 +10,21 @@ public class TesteFX {
 	public static void main(String[] args) {
 	
 		Controlador controlador = Controlador.getInstancia();
-		UsuarioTerraplanista user = controlador.pesquisarPorLogin("adm");
-		user.addAmigo(controlador.pesquisarPorLogin("roro"));
-		user.addAmigo(controlador.pesquisarPorLogin("bb"));
-		controlador.pesquisarPorLogin("roro").addAmigo(controlador.pesquisarPorLogin("rororo"));
-		controlador.pesquisarPorLogin("bb").addAmigo(controlador.pesquisarPorLogin("sdf"));
+		//UsuarioTerraplanista user = new UsuarioTerraplanista(new ArrayList<>(), new ArrayList<>(), "José de Paula", "reginaldo", "123", null, LocalDateTime.now(), null, 50, true);
+		//controlador.adicionarUsuario(user);
+		//user.addAmigo(controlador.pesquisarPorLogin("romulin"));
+		//user.addAmigo(controlador.pesquisarPorLogin("adm2"));
+		//user.addAmigo(controlador.pesquisarPorLogin("123"));
+		//controlador.salvar();
+		//user.addAmigo(controlador.pesquisarPorLogin("bb"));
+		//controlador.pesquisarPorLogin("roro").addAmigo(controlador.pesquisarPorLogin("rororo"));
+		//controlador.pesquisarPorLogin("bb").addAmigo(controlador.pesquisarPorLogin("sdf"));
 		
-		List<UsuarioTerraplanista> recomendados = controlador.indicacaoAmigoComum(controlador.pesquisarPorLogin("adm"));
+		//List<UsuarioTerraplanista> recomendados = controlador.indicacaoAmigoComum(user);
 		//List<UsuarioTerraplanista> recomendados = controlador.indicacaoPorInteresse(controlador.pesquisarPorLogin("adm"));
-		System.out.println(recomendados);
-		System.out.println(controlador.pesquisarPorLogin("adm").getAmigos());
-		System.out.println(controlador.pesquisarPorLogin("roro").getAmigos());
+		//System.out.println(recomendados);
+		//System.out.println(user.getAmigos());
+		//System.out.println(controlador.pesquisarPorLogin("roro").getAmigos());
+		System.out.println(controlador.recomendacoesPara(controlador.pesquisarPorLogin("adm")));
 	}
 }
